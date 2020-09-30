35398ec1d26e61386c99788f368afa6584f175c3
Juan Ignacio Márquez <ignaciomarquezc@gmail.com>
2020-09-30 14:08:15 -0300

    chore(): add connect-history-api-fallback package

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ const router = new VueRouter({
   routes: [
     { path: '/', component: Home },
     { path: '/terminos-y-condiciones', component: Terms },
+    { path: '*', component: Home }
   ]
 });
 
